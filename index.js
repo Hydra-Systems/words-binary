06b2dda1fee0a52ffacd6cc775cfc93b17974d41
Luís Felipe do Nascimento <luisfelipe.assuncao@hotmail.com>
2020-09-21 21:14:45 -0400

    Update index.js

--- index.js
+++ index.js
@@ -5,15 +5,5 @@ module.exports = function (str) {
   .replace(/[\u0300-\u036f]/g, '')
   .replace(/ /gm, '');
   
-  // Variavel que irá guardar o resultado
-  let result = ""
-
-  // Itera sobre o normalized-str
-  for(let i = 0; i < normalized_str.length; i++) {
-    let binaryChar = normalized_str.charCodeAt(i).toString(2)
-    result += binaryChar.length  === 8 ? binaryChar : '0'.repeat( 8 - binaryChar.length) + binaryChar
-  }
-
-  // Retorna o resultado
-  return result
+  return Array.from(normalized_str).map((word) => word.charCodeAt(0).toString(2)).join("")
 }
