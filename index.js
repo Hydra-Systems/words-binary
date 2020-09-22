b6e65c891d0a52f4e23bce4948c36e2494f75190
Toledo <lucastoledoaraujo@hotmail.com>
2020-09-22 08:55:06 -0300

    1.1.0 version

--- index.js
+++ index.js
@@ -1,19 +1,22 @@
 module.exports = function (str) {
   // Normaliza a string
   let normalized_str = str
-  .normalize('NFD')
-  .replace(/[\u0300-\u036f]/g, '')
-  .replace(/ /gm, '');
-  
+    .normalize('NFD')
+    .replace(/[\u0300-\u036f]/g, '')
+    .replace(/ /gm, '')
+
   // Variavel que irá guardar o resultado
-  let result = "";
+  let result = ''
 
   // Itera sobre o normalized-str
-  for(let i = 0; i < normalized_str.length; i++) {
-    let binaryChar = normalized_str.charCodeAt(i).toString(2);
-    result += binaryChar.length  === 8 ? binaryChar : '0'.repeat( 8 - binaryChar.length) + binaryChar;
+  for (let i = 0; i < normalized_str.length; i++) {
+    let binaryChar = normalized_str.charCodeAt(i).toString(2)
+    result +=
+      binaryChar.length === 8
+        ? binaryChar
+        : '0'.repeat(8 - binaryChar.length) + binaryChar
   }
 
   // Retorna o resultado
-  return result;
+  return result
 }
